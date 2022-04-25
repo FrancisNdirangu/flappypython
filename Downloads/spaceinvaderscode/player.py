1ca84b3251e9146662599744c14260d7883fd2a9
FrancisNdirangu <frankngugi@gmail.com>
2022-04-25 12:41:13 -0500

    printing shoot laser when space is pressed

--- Downloads/spaceinvaderscode/player.py
+++ Downloads/spaceinvaderscode/player.py
@@ -9,6 +9,9 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(midbottom=pos)
         self.speed = speed
         self.max_x_constraint = constraint
+        self.ready = True
+        self.laser_time = 0
+        self.laser_cooldown = 600
 
     def get_input(self):
         key = pygame.key.get_pressed()
@@ -18,11 +21,25 @@ class Player(pygame.sprite.Sprite):
         elif key[pygame.K_LEFT]:
             self.rect.x -= self.speed
 
+        if key[pygame.K_SPACE]:
+            self.shoot_laser()
+            self.ready = False
+            self.laser_time = pygame.time.get_ticks()
+
+        
+    
+
+
     def constraint(self):
         if self.rect.left <= 0:
             self.rect.left = 0
         elif self.rect.right >= self.max_x_constraint:
             self.rect.right = self.max_x_constraint
+
+    def shoot_laser(self):
+        print('shoot laser')
+
+
     def update(self):
         self.constraint()
         self.get_input()
