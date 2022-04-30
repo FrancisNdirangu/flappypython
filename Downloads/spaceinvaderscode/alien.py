fbb9070b5320886f38c3997b0bbcd0ea0fb6d6ca
FrancisNdirangu <frankngugi@gmail.com>
2022-04-30 15:40:26 -0500

    aliens now move horizontally and when they reach the edge the move a few pixels downwards

--- Downloads/spaceinvaderscode/alien.py
+++ Downloads/spaceinvaderscode/alien.py
@@ -6,3 +6,6 @@ class Alien(pygame.sprite.Sprite):
         file_path = 'C:/Users/franc/Downloads/spaceinvaderscode/Space-invaders-main/graphics/'+color+'.png'
         self.image = pygame.image.load(file_path).convert_alpha()
         self.rect = self.image.get_rect(topleft = (x,y))
+
+    def update(self,direction):
+        self.rect.x += direction
